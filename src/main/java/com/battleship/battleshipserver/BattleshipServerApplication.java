6ad9d50fd48c22b638d48463b9445edb4bede4d4
Astha Jain <astjain@adobe.com>
2017-05-25 01:01:20 +0530

    First commit

---
+++ src/main/java/com/battleship/battleshipserver/BattleshipServerApplication.java
@@ -0,0 +1,12 @@
+package com.battleship.battleshipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipServerApplication.class, args);
+	}
+}
